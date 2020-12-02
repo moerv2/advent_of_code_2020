0faa0927bdf018a9f116247b6d74c41792b9b148
unknown <marvin.vey@freenet.de>
2020-12-03 00:28:20 +0100

    Day 2

---
+++ 02/PasswordPhilosophy/PasswordPhilosophy/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace PasswordPhilosophy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+
+        public static void Part1()
+        {
+            var input = File.ReadAllLines("input.txt");
+            int counter = 0;
+            foreach (string str in input)
+            {
+                string[] substrings = str.Split('-', ':', ' ');
+                int min = Convert.ToInt32(substrings[0]);
+                int max = Convert.ToInt32(substrings[1]);
+                char character = Convert.ToChar(substrings[2]);
+                int charcount = substrings[4].Count(x => x == character);
+
+                if (charcount >= min && charcount <= max)
+                {
+                    counter += 1;
+                }
+            }
+
+            Console.WriteLine($"{counter} passwords are valid!");
+        }
+
+
+
+        public static void Part2()
+        {
+            var input = File.ReadAllLines("input.txt");
+            int counter = 0;
+            foreach (string str in input)
+            {
+                string[] substrings = str.Split('-', ':', ' ');
+                int min = Convert.ToInt32(substrings[0]);
+                int max = Convert.ToInt32(substrings[1]);
+                char character = Convert.ToChar(substrings[2]);
+                string password = substrings[4];
+
+                if (password[min - 1] == character && password[max - 1] != character)
+                {
+                    counter += 1;
+                }
+                else if (password[min - 1] != character && password[max - 1] == character)
+                {
+                    counter += 1;
+                }
+
+            }
+            Console.WriteLine($"{counter} passwords are valid!");
+        }
+    }
+}
